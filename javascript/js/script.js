0b2c0c7ec808ae9833aa2310c86602688629d6bb
Fantunesds <fantunesds@gmail.com>
2021-07-16 16:58:00 -0300

    commit

---
+++ javascript/js/script.js
@@ -0,0 +1,12 @@
+
+/***Fluxo de controle */
+
+//var theNumber = Number(prompt("Pick a number",""));
+//alert("your number is the square root of:  " + theNumber * theNumber);
+
+/**Execução Condicional */
+
+var thenumber = Number (prompt("Escolha um Número?",""));
+if(!isNaN(theNumber)){
+    alert("Seu número é a raiz quadrada de : " + theNumber * theNumber);
+}
